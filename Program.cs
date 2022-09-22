963e1c8292352b953346e8d9c59775a8a4264ab1
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-22 16:54:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Jeremy Gallen's sample program to demonstrate exceptions
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare array of fantasy authors
+            string[] authors = { "J.K. Rowling", "Terry Goodkind", "Robert Jordan", "Anne McCaffrey", "Terry Brooks" };
+
+            // Output authors
+            for(int i = 0; i < authors.Length; i++)
+            {
+                Console.WriteLine(authors[i]);
+            }
+
+            // Attempt to print value out of array range
+            try
+            {
+                Console.WriteLine(authors[7]);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("An exception has occurred: {0}", e.Message);
+            }
+        }
+    }
+}
